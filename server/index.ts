3875aedd7ebcea0e032f5ebb2253ca369313ec73
Vanildo de Souza <vanildo@skale.club>
2026-03-04 22:19:33 -0500

    fix(api): rename server routes file to avoid ESM path collision

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
+import { registerRoutes } from "./app-routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 import { logConfigStatus } from "./config";
